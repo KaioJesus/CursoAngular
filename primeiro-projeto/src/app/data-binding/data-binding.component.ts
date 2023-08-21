a2be4c40b89f69cb1aa3255b45c2f45ba45bf46f
Kaio <kaioramos990@hotmail.com>
2023-08-21 10:36:33 -0300

    seção data-binding completa

--- primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.scss']
 })
 export class DataBindingComponent implements OnInit {
-public nome: string = "Kaio";
-public idade: number = 23;
-public multiplicador: number = 2;
+  public nome: string = "Kaio";
+  public idade: number = 23;
+  public multiplicador: number = 2;
+
+  public checkedDisabled: boolean = false;
+  public imgSrc: string ="https://www.devmedia.com.br/arquivos/cursos/curso_o_que_e_angular_2312.jpg"
+  public imgTitle: string = "Curso angular - property binding"
+  public descricaoIMG: string = "Imagem o que é angular"
+
+  public position: {x: number, y:number} = {x: 0, y: 0}
 
   constructor(){}
   ngOnInit(): void {}
+
+  public alertaInfo(valor: MouseEvent){ //parâmetro tipado
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent){
+    // console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
